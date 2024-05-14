0b320b0277f615288e3d8361e235774db1f0ba3f
muhammed <ajferaju9961@gmail.com>
2024-05-14 15:09:25 +0530

    modified privateRoute

--- client/src/Utils/PrivateRoute.tsx
+++ client/src/Utils/PrivateRoute.tsx
@@ -1,35 +1,41 @@
 import { Outlet, Navigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { userState } from "../Types/Types";
+import { clearStore } from "../Redux/user";
 
 interface PrivateRouteProps {
   role: string;
 }
 
-export const PrivateRoute: React.FC<PrivateRouteProps> = ({ role }) => {
-  const userAuthenticated = localStorage.getItem("token") ? true : false;
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ role }) => {
+  const userAuthenticated = !!localStorage.getItem("token");
+  const dispatch = useDispatch();
   const isAdmin = useSelector((state: { user: userState }) => state.user.admin);
 
-  //check whether authenticated user and role(route trying to access) are the same
   const sameRole =
-    isAdmin && role == "admin"
+    isAdmin && role === "admin"
       ? true
-      : !isAdmin && role == "user"
+      : !isAdmin && role === "user"
       ? true
       : false;
 
+  useEffect(() => {
+    if (!userAuthenticated || !sameRole) {
+      setTimeout(() => {
+        toast.error("please login");
+      }, 1000);
+      dispatch(clearStore());
+      localStorage.removeItem("token");
+    }
+  }, [userAuthenticated, sameRole, dispatch]);
+
   if (!userAuthenticated || !sameRole) {
-    setTimeout(() => {
-      toast.error("please login");
-    }, 1000);
     return <Navigate to={role === "admin" ? "/admin/login" : "/login"} />;
   }
 
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
+
+export default React.memo(PrivateRoute);
